e7fb558d2c9725a9b1684d44d0dee5b1dddfa004
GeniusCodes007 <geniuscodes007@gmail.com>
2025-11-04 23:32:26 +0100

    Bible App

--- Bible_Content/GoodNews/m.py
+++ Bible_Content/GoodNews/m.py
@@ -1,4 +1,10 @@
 import pandas as pan
+try:
+    bn = pan.read_csv('chapterOne.csv', header=None)
+    #bn.columns = ['verse', 'verse content']
 
-bn = pan.read_csv('chapterOne.csv')
-print(bn.columns)
+    with open('chapterOne.csv', 'r') as chap_1:
+        for index, line in enumerate(chap_1.readlines()):
+            print(f"{index+1}: {line}")
+except Exception as e:
+    print(f"The issue is {e}")
